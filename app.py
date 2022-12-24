a9596acf90f8895aa3d2aa1c0b9f73e8fab50f62
fazlullahb0 <fazlullahb0@gmail.com>
2022-12-25 01:24:08 +0530

    api

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request
 
-
-
 app = Flask(__name__)
 
 
@@ -37,4 +35,20 @@ def create_item(name):
             new_item = {"name": request_data["name"], "price":request_data["price"]}
             store["items"].append(new_item) 
             return new_item, 201
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store_data(name): 
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"item": store["items"]}
     return {"message": "Store not found"}, 404
